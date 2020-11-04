df49163724f690e4bd3f077a6a3b1ef39e18ae6c
masahisa.hashimoto <changshangq@gmail.com>
2020-11-04 13:53:55 +0000

    create flash add create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,15 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(message_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に追加されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が追加されませんでした'
+      render :new
+    end
   end
 
   def edit
@@ -21,5 +30,9 @@ class TasksController < ApplicationController
   end
 
   def destroy
+  end
+  
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
